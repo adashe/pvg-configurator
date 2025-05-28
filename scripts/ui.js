224ad8642c48d2f76f3176e024f087dbbb65a059
Andrea Dashe <adashe@scc.lan>
2025-05-28 12:13:10 -0400

    updated assembly display page

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,21 +1,15 @@
-/* const hpuStart = document.querySelector('.hpu-start'); */
-/* const msStart = document.querySelector('.ms-start');
-const hmiStart = document.querySelector('.gw-start'); */
 
-/* const componentDiv = document.querySelector('#component-div'); */
-const hpuDiv = document.querySelector('#hpu-div');
-/* const mspDiv = document.querySelector('#msp-div');
-const hmiDiv = document.querySelector('#hmi-div'); */
+const pvgSetupDiv = document.querySelector('#pvg-setup-div');
 const partNumDiv = document.querySelector('#part-num-div');
 const contactDiv = document.querySelector('#contact-div');
-/* const emailConfDiv = document.querySelector('#email-conf-div'); */
+
 
 let currConfigurator = null;
 
 
 // Display component div
 const displayComponentDiv = () => {
-    hpuDiv.style.display = 'none';
+    pvgSetupDiv.style.display = 'none';
     partNumDiv.style.display = 'none';
     contactDiv.style.display = 'none';
 /*     emailConfDiv.style.display = 'none'; */
@@ -23,7 +17,7 @@ const displayComponentDiv = () => {
 
 // Display part number div
 const displayPartNumDiv = () => {
-    hpuDiv.style.display = 'none';
+    pvgSetupDiv.style.display = 'none';
     partNumDiv.style.display = 'block';
     contactDiv.style.display = 'none';
     /* emailConfDiv.style.display = 'none'; */
@@ -31,7 +25,7 @@ const displayPartNumDiv = () => {
 
 // Display email div
 const displayContactDiv = () => {
-    hpuDiv.style.display = 'none';
+    pvgSetupDiv.style.display = 'none';
     partNumDiv.style.display = 'none';
     contactDiv.style.display = 'block';
     /* emailConfDiv.style.display = 'none'; */
@@ -42,7 +36,7 @@ const displayContactDiv = () => {
 
 // Display email confirmation div
 const displayEmailConfDiv = () => {
-    hpuDiv.style.display = 'none';
+    pvgSetupDiv.style.display = 'none';
     partNumDiv.style.display = 'none';
     contactDiv.style.display = 'none';
     /* emailConfDiv.style.display = 'block'; */
--- scripts/valveForm.js
+++ scripts/valveForm.js
@@ -1,9 +1,4 @@
-const valvePortSizeDiv = document.querySelector('#valve-port-size-div');
-const valveSolVoltDiv = document.querySelector('#valve-sol-volt-div');
-const solenoidVoltage = document.querySelector('#SolenoidVoltage');
-
 const valvePopupWrapper = document.querySelector('.valve-popup-wrapper');
-/* const valvePopupCloseButtonX = document.querySelector('.valve-popup-close-x'); */
 const valvePopupContent = document.querySelector('.valve-popup-content');
 const valvePopupForm = document.querySelector('#valve-popup-form');
 
@@ -42,7 +37,7 @@ function generatePvgSelections(){
 
     pvgSelectionsContent.innerHTML = '';
 
-    let numSections = 12;
+    let numSections = pvgInputs.numSections;
 
 
     for(i = 0; i < numSections; i++){
@@ -93,72 +88,12 @@ function generatePvgSelections(){
 valvePopupForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    /* updateValvesAndHPU(); */
-
-    updateHpuDiv();
+    updatePvgAssem();
 
     valvePopupWrapper.style.display = 'none';
 });
 
-// Build valve assembly object from valve form inputs 
-async function addValveInputsToValveAssembly(){
 
-    let counter = []
 
-    for(i = 0; i < valveInputs.numStat; i++){
-        counter.push(i);
-    };
-
-    let promises = [];
-
-    // create a stations object for each submitted set of values
-    for await (i of counter){
-        let stationName = `station${i}`;
-        let valveID = `valve${i}`;
-        let flowControlID = `flowControl${i}`;
-        let checkValveID = `checkValve${i}`;
-
-        let valve = document.getElementById(valveID);
-        let flowControl = document.getElementById(flowControlID);
-        let checkValve = document.getElementById(checkValveID);
-
-        // Assign value of 0 to null (disabled) flow control and check valve options
-        let flowControlValue = flowControl.value;
-        
-        if(!flowControl.value){
-            flowControlValue = 0
-        };
-
-        let checkValveValue = checkValve.value;
-        if(!checkValve.value){
-            checkValveValue = 0
-        };
-
-        let promise = valveAssem.updateStation(stationName, valve.value, flowControlValue, checkValveValue);
-    
-        promises.push(promise);
-    };
-
-    await Promise.all(promises);
-
-    // Update voltage attribute based on the solenoid voltage selection
-    valveAssem.voltage = valveInputs.solVolt;
-
-    // Update number of L valves (for hpu calc)
-    valveAssem.countLValves();
-
-    // Update number of flow controls (for hpu calc)
-    valveAssem.countFlowControl();
-};
-
-// Update valve and HPU number display when edits are made from the display page, including updating the heat exchanger if necessary
-async function updateValvesAndHPU(){
-
-    await addValveInputsToValveAssembly();
-
-    if(partNumDiv.style.display == 'block'){
-        await updateHpuDiv();
-    };
-};
 
 
